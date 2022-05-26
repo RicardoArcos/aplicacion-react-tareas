b181b5ba5706d4df1667a4ab9fb1466844e65284
Ricardo Arcos <ingricardoarcos@gmail.com>
2022-05-26 13:22:41 -0500

    agregando más eventos

--- src/componentes/listaTareas.js
+++ src/componentes/listaTareas.js
@@ -15,6 +15,21 @@ function ListaTareas(){
     }
   }
 
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id == id){
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  }
+
   return(
     <>
       <TareaFormulario 
@@ -28,6 +43,8 @@ function ListaTareas(){
               id={tarea.id}
               texto={tarea.texto}
               completada = {tarea.completada}
+              eliminarTarea = {eliminarTarea}
+              completarTarea = {completarTarea}
             />
           )
         }
